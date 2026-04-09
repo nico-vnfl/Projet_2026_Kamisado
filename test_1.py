d70652ba5884e535b465219c41c6dc80f9390646
Libz <Theo.libert@icloud.com>
2026-04-09 14:24:28 +0200

    connexion en local

--- test_1.py
+++ test_1.py
@@ -5,9 +5,17 @@ import threading
 
 HOST = "172.17.89.143"
 PORT = 3000
-
 CLIENT_PORT = 8888
 
+
+def recvall(sock, n):
+    data = b""
+    while len(data) < n:
+        packet = sock.recv(n - len(data))
+        if not packet:
+            return None
+        data += packet
+    return data
 
 
 def start_server():
@@ -21,47 +29,55 @@ def start_server():
             while True:
                 client, addr = s.accept()
                 with client:
-                    
-                    raw_len = client.recv(4)
-                    msg_len = struct.unpack("!I", raw_len)[0]
 
-                    data = client.recv(msg_len)
+                    raw_len = recvall(client, 4)
+                    if not raw_len:
+                        continue
+
+                    msg_len = struct.unpack("I", raw_len)[0]
+
+                    data = recvall(client, msg_len)
+                    if not data:
+                        continue
+
                     msg = json.loads(data.decode())
 
-                    print("\n[RECU DU SERVEUR]", msg)
+                    print("[RECU DU SERVEUR]", msg)
 
-                    
                     if msg["request"] == "ping":
                         response = {"response": "pong"}
+
                         resp_data = json.dumps(response).encode()
-                        packet = struct.pack("!I", len(resp_data)) + resp_data
+                        packet = struct.pack("I", len(resp_data)) + resp_data
+
                         client.sendall(packet)
 
-    threading.Thread(target=handler, daemon=True).start()
+    thread = threading.Thread(target=handler, daemon=True)
+    thread.start()
 
 
+def subscribe():
+    msg = {
+        "request": "subscribe",
+        "port": CLIENT_PORT,
+        "name": "Theo",
+        "matricules": ["12345"]
+    }
 
+    data = json.dumps(msg).encode()
+    packet = struct.pack("I", len(data)) + data
+
+    with socket.socket() as s:
+        s.connect((HOST, PORT))
+        s.sendall(packet)
+
+    print("INSCRIPTION ENVOYEE")
+
+
+# Lancement
 start_server()
+subscribe()
 
-
-
-msg = {
-    "request": "subscribe",
-    "port": CLIENT_PORT,
-    "name": "Theo",
-    "matricules": ["12345"]
-}
-
-data = json.dumps(msg).encode()
-packet = struct.pack("I", len(data)) + data
-
-with socket.socket() as s:
-    s.connect((HOST, PORT))
-    s.sendall(packet)
-
-print("INSCRIPTION ENVOYÉE")
-
-
-
+# Boucle pour garder le programme actif
 while True:
     pass
